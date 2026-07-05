633b259340481b209784ddd417b318378485f172
Yash4518 <sarfareyash002@gmail.com>
2026-07-05 20:46:48 +0530

    Modified App.java for second job testing

--- src/main/java/com/acc/App.java
+++ src/main/java/com/acc/App.java
@@ -6,6 +6,6 @@ package com.acc;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello Jenkins!, testing second job");
     }
 }
